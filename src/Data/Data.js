0a5ec178413d88241d96283137bfe058ded5ce15
unknown <you@example.com>
2023-06-22 09:04:50 +0200

    add import and cards

---
+++ src/Data/Data.js
@@ -0,0 +1,9 @@
+const data = [
+
+    {id:1 , Name:"Bread", Price:"", image:""},
+    {id:1 , Name:"Cheese", Price:"", image:""},
+    {id:1 , Name:"Russian", Price:"", image:""},
+    {id:1 , Name:"Rama", Price:"", image:""},
+    {id:1 , Name:"Chips", Price:"", image:""},
+]
+export default data
