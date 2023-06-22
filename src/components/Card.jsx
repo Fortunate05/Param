955aa4e9536210fe27005a8e84215de4bbb93e31
unknown <you@example.com>
2023-06-22 09:35:49 +0200

    add linked in html and install

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card({ props }) {
             {
                 props.map((items, index) =>
 
-                    <div className="card" style="width: 18rem;">
+                    <div className="card" style={{"width: 18rem;"}}>
                         <img className="card-img-top" src="..." alt="Card image cap"/>
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
